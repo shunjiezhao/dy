19d4c096dee3e0f9d8e3b9e73f31a07b5abc0158
zsj <939038111@qq.com>
2023-01-24 20:39:56 +0800

    🚧 完成mysql docker file 的书写

--- pkg/constants/constant.go
+++ pkg/constants/constant.go
@@ -8,10 +8,9 @@ const (
 	SecretKey      = "secret key"
 	IdentityKey    = "user_id"
 	PublicKeyFile  = "E:\\DY2023\\pkg\\constants\\pub.key"
-	PrivateKeyFile = "E:\\DY2023\\pkg\\constants\\private.key"
+	PrivateKeyFile = ".\\pkg\\constants\\private.key"
 
-	MySQLDefaultDSN = "dy:123456@tcp(localhost:3307)/dy?charset=utf8mb4&parseTime=True&loc=Local"
-	//MySQLDefaultDSN   = "zsj:az123.@tcp(localhost:3306)/dy?charset=utf8mb4&parseTime=True&loc=Local"
+	MySQLDefaultDSN   = "dy:123456@tcp(localhost:3307)/dy?charset=utf8mb4&parseTime=True&loc=Local"
 	EtcdAddress       = "127.0.0.1:2379"
 	ApiServerAddress  = "127.0.0.1:8888"
 	UserServerAddress = "127.0.0.1:8889"
